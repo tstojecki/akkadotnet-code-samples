2fdda915d549e1f88a933529a1b68431178cad81
Tomek Stojecki <tstojecki@gmail.com>
2015-06-16 12:08:29 +0200

    Actor system in webapi
    Added a new webapi project and moved the actor system to it. Removed
    server side code from mvc, runs index.html to (spa) instead.

---
+++ Cluster.WebCrawler/src/WebCrawler.WebApi/Actors/SystemActors.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+
+namespace WebCrawler.WebApi.Actors
+{
+    /// <summary>
+    /// Static class used to work around weird SignalR constructors
+    /// 
+    /// (need to learn how to wire this up properly in signalr)
+    /// </summary>
+    public static class SystemActors
+    {
+        public static IActorRef SignalRActor = ActorRefs.Nobody;
+
+        public static IActorRef CommandProcessor = ActorRefs.Nobody;
+    }
+}
---
+++ Cluster.WebCrawler/src/WebCrawler.WebApi/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using Owin;
+using WebCrawler.Messages.Actors;
+using WebCrawler.WebApi.Actors;
+
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(WebCrawler.WebApi.Startup), "ApplicationShutdown")]
+
+namespace WebCrawler.WebApi
+{
+    public class Startup
+    {
+        protected static ActorSystem ActorSystem;
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            CreateActorSystem();
+        }
+
+        private void CreateActorSystem()
+        {
+            ActorSystem = ActorSystem.Create("webcrawler");
+
+            var router = ActorSystem.ActorOf(Props.Create(() => new RemoteJobActor()).WithRouter(FromConfig.Instance), "tasker");
+            SystemActors.CommandProcessor = ActorSystem.ActorOf(Props.Create(() => new CommandProcessor(router)),
+                "commands");
+            SystemActors.SignalRActor = ActorSystem.ActorOf(Props.Create(() => new SignalRActor()), "signalr");
+        }
+    }
+}
